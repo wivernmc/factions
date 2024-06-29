271b5f894f52c251c615bc0fb7b5e667d00471ed
Driftay <driftayllc@gmail.com>
2024-06-29 09:07:58 -0400

    Converted Hashmap To ConcurrentHashmap for Thread-Safety

--- src/main/java/com/massivecraft/factions/zcore/util/DiscUtil.java
+++ src/main/java/com/massivecraft/factions/zcore/util/DiscUtil.java
@@ -3,32 +3,25 @@ package com.massivecraft.factions.zcore.util;
 import com.massivecraft.factions.FactionsPlugin;
 import org.bukkit.Bukkit;
 
-import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class DiscUtil {
 
-    private static final HashMap<String, Lock> LOCKS = new HashMap<>();
+    private static final Map<String, Lock> LOCKS = new ConcurrentHashMap<>();
 
     public static byte[] readBytes(Path path) throws IOException {
         return Files.readAllBytes(path);
     }
 
     public static void writeBytes(Path path, byte[] bytes) throws IOException {
-        if (!Files.exists(path)) {
-            Files.createFile(path);
-        }
-
-        try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(path))) {
-            out.write(bytes);
-        }
+        Files.write(path, bytes);
     }
 
     public static void write(Path path, String content) throws IOException {
@@ -43,7 +36,7 @@ public class DiscUtil {
         String name = path.getFileName().toString();
         Lock lock = LOCKS.computeIfAbsent(name, n -> new ReentrantReadWriteLock().writeLock());
 
-        Runnable write = () -> {
+        Runnable writeTask = () -> {
             lock.lock();
             try {
                 write(path, content);
@@ -55,9 +48,9 @@ public class DiscUtil {
         };
 
         if (sync) {
-            write.run();
+            writeTask.run();
         } else {
-            Bukkit.getScheduler().runTaskAsynchronously(FactionsPlugin.getInstance(), write);
+            Bukkit.getScheduler().runTaskAsynchronously(FactionsPlugin.getInstance(), writeTask);
         }
         return true;
     }
@@ -66,6 +59,7 @@ public class DiscUtil {
         try {
             return read(path);
         } catch (IOException e) {
+            e.printStackTrace();
             return null;
         }
     }
